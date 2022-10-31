042818318e6d52b66ccd975b34fa734118f17243
Tchomasek <46090576+Tchomasek@users.noreply.github.com>
2022-10-31 13:55:45 +0100

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,12 +10,12 @@ import { products } from '../products';
 export class ProductListComponent {
   products = [...products];
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product: any) {
+    window.alert(`The ${product.name} has been shared!`);
   }
 
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale)
+  onNotify(product: any) {
+    window.alert(`You will be notified when the ${product.name} goes on sale`);
   }
 }
 
